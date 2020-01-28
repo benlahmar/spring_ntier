483846075f3e66fa2eb6d6a8dfb4c0edba9c3bbf
moi <moi@pc-moi>
2020-01-28 12:22:28 +0000

    service categ

---
+++ EStore/src/com/entities/dao/MainTest.java
@@ -0,0 +1,20 @@
+package com.entities.dao;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.entities.Categorie;
+import com.entities.services.IserviceCat;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx2=new ClassPathXmlApplicationContext("Beans.xml");
+
+		IserviceCat	pm= (IserviceCat) ctx2.getBean("servicecat");
+		Categorie c=new Categorie();
+		c.setLibelle("accessoires");
+		pm.AddCategorie(c);
+	}
+
+}
